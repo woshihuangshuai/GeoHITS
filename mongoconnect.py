b787df6d37d984f56effee7b5613cc16b86e6c15
yiyishiwo <woshihuangshuai@hotmail.com>
2016-03-17 11:43:25 +0800

    This file is a test file and useless.

--- mongoconnect.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from pymongo import MongoClient
-import re
-client = MongoClient('localhost',27017)
-db = client['test']
-collection = db['dataset-1']
-
-item = {"text" : "Hello Mongo!"}
-collection.insert(item)
